cd4d39cce1bd5389e1c9045ee687110174a8ad96
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-04-16 03:04:06 -0400

    removed unused application

--- evara-backend/nav.js
+++ evara-backend/nav.js
@@ -15,38 +15,20 @@ $(document).ready(function () {
                 <span class="text">Promocode</span>
             </a>
         </li>
-        <li id="product" class="menu-item has-submenu">
+        <li id="product" class="menu-item">
             <a class="menu-link" href="page-form-product-1.html"> <i class="icon material-icons md-add_box"></i>
                 <span class="text">Add product</span>
             </a>
-            <div class="submenu">
-                <a href="page-form-product-1.html">Add product 1</a>
-                <a href="page-form-product-2.html">Add product 2</a>
-                <a href="page-form-product-3.html">Add product 3</a>
-                <a href="page-form-product-4.html">Add product 4</a>
-            </div>
         </li>
-        <li id="order" class="menu-item has-submenu">
+        <li id="order" class="menu-item">
             <a class="menu-link" href="page-orders-1.html"> <i class="icon material-icons md-shopping_cart"></i>
                 <span class="text">Orders</span>
             </a>
-            <div class="submenu">
-                <a href="page-orders-1.html">Order list 1</a>
-                <a href="page-orders-2.html">Order list 2</a>
-                <a href="page-orders-detail.html">Order detail</a>
-                <a href="page-orders-tracking.html">Order tracking</a>
-                <a href="page-invoice.html">Invoice</a>
-            </div>
         </li>
-        <li class="menu-item has-submenu">
+        <li class="menu-item">
             <a class="menu-link" href="page-transactions-1.html"> <i class="icon material-icons md-monetization_on"></i>
                 <span class="text">Transactions</span>
             </a>
-            <div class="submenu">
-                <a href="page-transactions-1.html">Transaction 1</a>
-                <a href="page-transactions-2.html">Transaction 2</a>
-                <a href="page-transactions-details.html">Transaction Details</a>
-            </div>
         </li>
         <li class="menu-item">
             <a class="menu-link" href="page-reviews.html"> <i class="icon material-icons md-comment"></i>
